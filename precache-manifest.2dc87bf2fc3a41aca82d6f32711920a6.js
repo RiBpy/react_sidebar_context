e78b5ce690eec771ed5477b27ae0df8013cabffb
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-21 20:19:47 +0600

    Updates

---
+++ precache-manifest.2dc87bf2fc3a41aca82d6f32711920a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df26e464448bf4116e32fff5a6a2271",
+    "url": "/sidebar_globalState/index.html"
+  },
+  {
+    "revision": "0f029c138d5a62b35c96",
+    "url": "/sidebar_globalState/static/css/main.2ee9bded.chunk.css"
+  },
+  {
+    "revision": "1f40ff754100d139d8c0",
+    "url": "/sidebar_globalState/static/js/2.1707915b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sidebar_globalState/static/js/2.1707915b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f029c138d5a62b35c96",
+    "url": "/sidebar_globalState/static/js/main.765c5987.chunk.js"
+  },
+  {
+    "revision": "b7b13e3c08700bde558d",
+    "url": "/sidebar_globalState/static/js/runtime-main.2efc6efc.js"
+  }
+]);
